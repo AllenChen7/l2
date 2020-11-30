f6582ca64667fcc93dcf9fdc25bbd35c8b045b17
chen <1175195335@qq.com>
2020-11-30 16:47:01 +0800

    发送短信验证码

---
+++ app/Http/Requests/Api/VerificationCodeRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+class VerificationCodeRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'phone' => [
+                'required',
+                'regex:/^1\d{10}$/',
+                'unique:users'
+            ]
+        ];
+    }
+}
